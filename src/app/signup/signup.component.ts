8a54c9fbe073296aa062575ab151fb2d140eb921
farinoor <farithanoormohammed@gmail.com>
2022-02-01 08:19:19 +0530

    forms

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  signupdata = {
+    user: 'umar',
+    password: 'umar@0786',
+  };
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  senduserdata(f: any): void {
+    // console.log(this.signupdata);
+    // this.http
+    //   .post('https://jsonplaceholder.typicode.com/comments', this.signupdata)
+    //   .subscribe((data: any) => {
+    //     console.log(data);
+    //   });
+    console.log(f);
+    console.log(f.value);
   }
-
 }
